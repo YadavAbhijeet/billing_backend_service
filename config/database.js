4471d21af89efb09257b7019480992c1c6d613af
Ajit yadav <yajit289@gmail.com>
2025-10-24 14:15:36 +0530

    config converted from postgres to Sql lite

--- config/database.js
+++ config/database.js
@@ -1,8 +1,20 @@
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('billing_service', 'billing_user1', 'billing_user1', {
-  host: 'localhost',
-  dialect: 'postgres',
+// const sequelize = new Sequelize('billing_service', 'billing_user1', 'billing_user1', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+// });
+
+// module.exports = sequelize;
+
+
+const { Sequelize } = require('sequelize');
+const path = require('path');
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: path.join(__dirname, 'database.sqlite'), // SQLite file
+  logging: console.log, // optional
 });
 
 module.exports = sequelize;
